df8a9d615b38fb4a4ca2ce858ae6b6f960001b23
shaunieboy31 <shaunieboy573@gmail.com>
2026-05-12 15:03:16 +0800

    Restructured Folder

--- app/(main)/contact/page.tsx
+++
@@ -1,7 +0,0 @@
-import { ContactService } from "@/lib/services/contact.service";
-import ContactPage from "./ContactPage";
-
-export default async function Page() {
-  const contactInfo = await ContactService.getInfo();
-  return <ContactPage contactInfo={contactInfo} />;
-}
